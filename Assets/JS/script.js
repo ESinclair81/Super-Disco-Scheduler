cfd865e1aa2d4051377b347adcb4b6bfa03357f0
Soul Pro <70108490+ESinclair81@users.noreply.github.com>
2020-10-03 20:55:08 -0400

    added first part of hour updater feature

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -14,5 +14,31 @@ $(document).ready(function() {
     
     });
 
+    //Function to update desrciption status every hour 
+      
+        //Getting the current hours
+    var currentHour = moment().hours();
+
+    //Time-block loop
+    $(".time-block").each(function() {
+        var blockHour = parseInt($(this).attr("id").split("-")[1]);
+    
+    //If time is in past
+        if (blockHour < currentHour ) {
+            $(this).addClass("past");
+        }
+        
+        else if (blockHour === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present"); 
+        }
+
+        else if (blockHour > currentHour) {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+
+    });
     
     })
